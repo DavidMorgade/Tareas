05974e00ce96fbc68939377a9b3c508ad9a7061e
DavidMorgade <davidmorgadegil@gmail.com>
2023-11-05 22:11:33 +0100

    añado actividad 1 tema 5 en csharp

---
+++ Programacion/TEMA5/Alumno.cs
@@ -0,0 +1,15 @@
+// Implementa una clase Alumno que represente a los alumnos de una asignatura. De cada alumno interesa
+// conocer su DNI, su edad y su nota de cada trimestre.
+// Crea las variables de la clase del tipo que creas más conveniente.
+// Para terminar, crea dos objetos de la clase Alumno.
+// Para el desarrollo de esta actividad debéis de crear un proyecto en NetBeans para realizar el ejercicio.
+// Recuerda que para crear una clase debes usar la palabra reservada class.
+
+public class Alumno {
+
+    //Atributos con getters y setters
+    private string DNI {get; set;};
+    private int edad {get; set;};
+    private int nota {get; set;};
+    
+}
---
+++ Programacion/TEMA5/TEMA_5_ACTIVIDAD_1.cs
@@ -0,0 +1,8 @@
+public class TEMA_5_ACTIVIDAD_1 {
+    public static void Main(string[] args) {
+
+        Alumno david = new Alumno();
+
+        
+    }
+}
